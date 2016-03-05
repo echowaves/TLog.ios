a9eea8657a62f3f27acea8f1be6c26b2b135cf85
dmitryame <dmitry@custommode.com>
2016-03-05 00:02:44 -0500

    lookup codes

--- TLog/PickActionCodeViewController.swift
+++ TLog/PickActionCodeViewController.swift
@@ -10,11 +10,33 @@ import Foundation
 
 
 
-class PickActionCodeViewController: UIViewController {
+class PickActionCodeViewController: UIViewController,UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource, UIPopoverPresentationControllerDelegate {
     
     
+    @IBOutlet weak var nextButton: UIBarButtonItem!
+    @IBOutlet weak var actionCodeTextField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    var completions = [TLActionCode]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        actionCodeTextField.becomeFirstResponder()
+        actionCodeTextField.delegate = self
+
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(
+            self,
+            selector: "textFieldTextChanged:",
+            name:UITextFieldTextDidChangeNotification,
+            object: actionCodeTextField
+        )
+        
+        
+        tableView.hidden        =   true
+        tableView.delegate      =   self
+        tableView.dataSource    =   self
+
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -24,6 +46,59 @@ class PickActionCodeViewController: UIViewController {
     @IBAction func backButtonClicked(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
+
+    @IBAction func nextButtonClicked(sender: AnyObject) {
+    }
+    
+    
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange,
+        replacementString string: String) -> Bool
+    {
+        let maxLength = 500
+        let currentString: NSString = actionCodeTextField.text!
+        let newString: NSString =
+        currentString.stringByReplacingCharactersInRange(range, withString: string)
+        return newString.length <= maxLength
+    }
+    
+    
+    func textFieldTextChanged(sender : AnyObject) {
+        NSLog("searching for text: " + actionCodeTextField.text!); //the textView parameter is the textView where text was changed
+        TLActionCode.autoComplete(actionCodeTextField.text!,
+            success: { (results) -> () in
+            
+            self.tableView.hidden = false
+            self.completions = results
+            self.tableView.reloadData()
+            self.tableView.reloadInputViews()
+            
+            }) { (error) -> () in
+                NSLog("error autocompleting")
+        }
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.completions.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell!
+        
+        cell.textLabel?.text = self.completions[indexPath.row].code! + ":" + self.completions[indexPath.row].descr!
+        cell.textLabel?.textColor = UIColor(rgb: 0xff8000)
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.hidden = true
+        NSLog("You selected cell #\(self.completions[indexPath.row])!")
+        actionCodeTextField.text = self.completions[indexPath.row].descr!
+    }
+    
 
     
 }
---
+++ TLog/TLActionCode.swift
@@ -0,0 +1,85 @@
+//
+//  TLActionCode.swift
+//  TLog
+//
+//  Created by D on 3/3/16.
+//  Copyright © 2016 EchoWaves. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class TLActionCode: NSObject {
+    
+    var id: Int?
+    var code:String?
+    var descr:String?
+    
+    init(id: Int!, code:String!, descr:String!) {
+        self.id = id
+        self.code = code
+        self.descr = descr
+    }
+
+    class func autoComplete(
+        var searchText: String,
+        success:(results:[TLActionCode]) -> (),
+        failure:(error: NSError!) -> ()
+        ) -> () {
+            searchText = searchText.lowercaseString
+            
+
+            Alamofire.request(.GET, "\(TL_HOST)/actioncodes/lookup/\(searchText)" , encoding: ParameterEncoding.JSON)
+                .validate(statusCode: 200..<300)
+                .responseJSON { response in
+                    switch response.result {
+                    case .Success:
+                        
+                        var actionCodes:[TLActionCode] = [TLActionCode]()
+                        
+                        let returnedCodes:NSArray = (response.result.value!["result"])!! as! NSArray
+                        
+                        for returnedCode in returnedCodes {
+                            actionCodes.append(TLActionCode(
+                                id:    returnedCode["id"]! as! Int,
+                                code:  returnedCode["code"]! as! String,
+                                descr: returnedCode["description"]! as! String
+                                )
+                            )
+                        }
+                        
+                        success(results: actionCodes)
+                    case .Failure(let error):
+                        NSLog(error.description)
+                        failure(error: error)
+                    }
+            }
+     
+        }
+
+    
+    
+//        class func getAllCheckins(
+//            activation_code: String,
+//            success:() -> (),
+//            failure:(error: NSError) -> ()) -> () {
+//                let headers = [
+//                    "Authorization": "Bearer \(TLUser.retreiveJwtFromLocalStorage())",
+//                    "Content-Type": "application/json"
+//                ]
+//                let parameters = ["name": name, "email": email]
+//                Alamofire.request(.PUT, "\(TL_HOST)/employees/\(employeeId)" , parameters: parameters, encoding: ParameterEncoding.JSON, headers: headers)
+//                    .validate(statusCode: 200..<300)
+//                    .responseJSON { response in
+//                        switch response.result {
+//                        case .Success:
+//                            success();
+//                        case .Failure(let error):
+//                            NSLog(error.description)
+//                            failure(error: error)
+//                        }
+//                }
+//        }
+    
+}
